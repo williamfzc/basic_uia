0e063f92b96a6b215862f48f5b5bc0f680e7f38c
William Feng <178894043@qq.com>
2018-06-07 15:39:19 +0800

    bug fix

--- basic_uia/logger.py
+++ basic_uia/logger.py
@@ -6,19 +6,16 @@ info = None
 
 
 def init_logger(logger_path):
-    logger = logging.getLogger()
     log_format = "%(asctime)s - %(levelname)s - %(message)s"
-
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(log_format)
-    console_handler.setLevel('INFO')
+    logging.basicConfig(
+        level=logging.INFO,
+        format=log_format,
+    )
 
     file_handler = logging.FileHandler(logger_path)
-    file_handler.setFormatter(log_format)
-    file_handler.setLevel('INFO')
-
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
+    file_handler.setFormatter(logging.Formatter(log_format))
+    file_handler.setLevel(logging.INFO)
+    logging.getLogger('').addHandler(file_handler)
 
     global info
     info = logging.info
@@ -27,8 +24,8 @@ def init_logger(logger_path):
 def add_log(func):
     @functools.wraps(func)
     def deco(*args, **kwargs):
-        logging.info(func.__name__ + 'start ...')
+        logging.info(func.__name__ + ' start ...')
         result = func(*args, **kwargs)
-        logging.info(func.__name__ + 'stop.')
+        logging.info(func.__name__ + ' stop.')
         return result
     return deco
