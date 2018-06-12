f2a8d848e68f9fdd44af9806f665e18219f873ac
williamfzc <178894043@qq.com>
2018-06-12 18:18:02 +0800

    添加临时的键盘输入方法

--- basic_uia/api.py
+++ basic_uia/api.py
@@ -4,6 +4,37 @@ import importlib
 import config as cf
 import basic_uia.logger as logger
 
+KEY_MAP = {
+    'q': 16,
+    'w': 17,
+    'e': 18,
+    'r': 19,
+    't': 20,
+    'y': 21,
+    'u': 22,
+    'i': 23,
+    'o': 24,
+    'p': 25,
+
+    'a': 30,
+    's': 31,
+    'd': 32,
+    'f': 33,
+    'g': 34,
+    'h': 35,
+    'j': 36,
+    'k': 37,
+    'l': 38,
+
+    'z': 44,
+    'x': 45,
+    'c': 46,
+    'v': 47,
+    'b': 48,
+    'n': 49,
+    'm': 50,
+}
+
 
 def load_extend_api(module_class):
     if os.path.exists(cf.EXTEND_API_FILE):
@@ -34,3 +65,13 @@ class CustomAPI(object):
         self.device(
             resourceId='com.coloros.recents:id/clear_button'
         ).click()
+
+    def single_type_input(self, single_letter):
+        letter = str(single_letter.lower())
+        hex_value = hex(KEY_MAP[letter])
+        self.device.press(hex_value)
+
+    def long_type_input(self, full_str):
+        target_str = str(full_str).lower()
+        for each_letter in list(target_str):
+            self.single_type_input(each_letter)
